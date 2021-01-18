a544cd79de694469cadb1055d2bfd4f1ea7c589f
GitHub Action <action@github.com>
2021-01-18 04:14:00 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/madwareru/intellijronremix/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.madwareru.intellijronremix.services
+
+import com.github.madwareru.intellijronremix.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/madwareru/intellijronremix/services/MyProjectService.kt
+++ src/main/kotlin/com/github/madwareru/intellijronremix/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.madwareru.intellijronremix.services
 
+import com.github.madwareru.intellijronremix.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
