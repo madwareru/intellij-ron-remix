0c24eb32b855252e8f4fde93a4fb61315e81299f
madwareru <madware.ru@gmail.com>
2021-01-25 09:45:20 +0300

    Ktlint are gonna die

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,6 @@ plugins {
     id("org.jetbrains.intellij") version "0.6.5"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.0.0"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
 // Import variables from gradle.properties file
