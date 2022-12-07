a313b5cb17df1ed3a7dbd0649de5226ce085f0e7
madwareru <madware.ru@gmail.com>
2022-12-07 15:00:00 +0400

    Update plugin version

--- src/main/kotlin/com/github/madwareru/intellijronremix/ide/codeStyle/RONCodeStyleSettings.kt
+++ src/main/kotlin/com/github/madwareru/intellijronremix/ide/codeStyle/RONCodeStyleSettings.kt
@@ -3,7 +3,7 @@ package com.github.madwareru.intellijronremix.ide.codestyle
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CustomCodeStyleSettings
 
-class RONCodeStyleSettings(settings: CodeStyleSettings?) : CustomCodeStyleSettings(
+class RONCodeStyleSettings(settings: CodeStyleSettings) : CustomCodeStyleSettings(
     "RONCodeStyleSettings",
     settings
 )
