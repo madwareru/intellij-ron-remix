cbb90a769c69ef6d914582989793bdf29c283152
Vitaly <madware.ru@gmail.com>
2022-12-07 18:44:36 +0300

    Update plugin version (#28)
    * Update plugin version
    
    * Update plugin version
    
    * Update plugin version
    
    * Update plugin version
    
    * Update plugin version
    
    * Update plugin version
    
    * Update plugin version
    
    * Update build.yml
    
    * Update plugin version
    
    * Update plugin version
    
    * Update release.yml
    
    * Update run-ui-tests.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.7.22"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.10.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.3.1"
 }
@@ -21,11 +21,9 @@ repositories {
     mavenCentral()
 }
 
-// Set the JVM language level used to compile sources and generate files - Java 11 is required since 2020.3
+// Set the JVM language level used to build project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
-    }
+    jvmToolchain (17)
 }
 
 sourceSets["main"].java.srcDirs("src/main/gen")
