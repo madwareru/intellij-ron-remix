645b39be035bb9f23e24f4b72e60932e1b3e8959
Lars Frost <freezinglava@proton.me>
2023-02-24 19:28:51 +0100

    Only complete `enable()` in extensions

--- src/main/kotlin/com/github/madwareru/intellijronremix/ide/completion/RONOptionCompletionContributor.kt
+++ src/main/kotlin/com/github/madwareru/intellijronremix/ide/completion/RONOptionCompletionContributor.kt
@@ -1,10 +1,15 @@
 package com.github.madwareru.intellijronremix.ide.completion
 
+import com.github.madwareru.intellijronremix.language.psi.RONExt
+import com.github.madwareru.intellijronremix.language.psi.RONExtensions
+import com.github.madwareru.intellijronremix.language.psi.RONObjectName
 import com.github.madwareru.intellijronremix.language.psi.RONTypes
+import com.github.madwareru.intellijronremix.language.psi.RONValue
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.editor.EditorModificationUtil
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.util.parentOfType
 import com.intellij.util.ProcessingContext
 
 class RONOptionCompletionContributor : CompletionContributor() {
@@ -18,13 +23,15 @@ class RONOptionCompletionContributor : CompletionContributor() {
                     context: ProcessingContext,
                     resultSet: CompletionResultSet
                 ) {
-                    resultSet.addElement(
-                        LookupElementBuilder
-                            .create("enable()")
-                            .withInsertHandler { ctx, _ ->
-                                EditorModificationUtil.moveCaretRelatively(ctx.editor, -1)
-                            }
-                    )
+                    if (parameters.position.parentOfType<RONExt>(false) != null) {
+                        resultSet.addElement(
+                            LookupElementBuilder
+                                .create("enable()")
+                                .withInsertHandler { ctx, _ ->
+                                    EditorModificationUtil.moveCaretRelatively(ctx.editor, -1)
+                                }
+                        )
+                    }
                     resultSet.addElement(
                         LookupElementBuilder
                             .create("Some()")
