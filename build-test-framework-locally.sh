8563e73e6283c8a4507a97cbde79e9d02e4e132e
Lars Frost <freezinglava@proton.me>
2023-07-29 13:39:43 +0200

    Extract build instruction bash script

---
+++ build-test-framework-locally.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/flash-freezing-lava/intellij-directory-tests.git
+cd intellij-directory-tests
+git checkout v0.2.0
+gradle wrapper
+./gradlew publishToLocalMaven
+cd ..
+rm -rf intellij-directory-tests
